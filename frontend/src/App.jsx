8402ae5f28fca2dcce18091d267e0fdf6277d30f
mr9 asif <asifalibd002@gmail.com>
2025-11-22 17:30:39 +0600

    added signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-import Home from './pages/Home'; // adjust path as needed
+import Home from './pages/Home'; // adjust 
+// path as needed
+import Signup from "./components/Signup";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup/>} />
     </Routes>
   );
 };
